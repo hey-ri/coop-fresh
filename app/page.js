cc9d7a553b1be59f00d0ac0de3884862b153c0ec
hey-ri <kwonhr01@naver.com>
2023-05-21 22:37:14 +0900

    create nav bar

--- app/page.js
+++ app/page.js
@@ -1,8 +1,14 @@
+import Link from 'next/link';
+
 export default function Home() {
   const name = 'KWON';
 
   return (
     <div>
+      <nav>
+        <Link href={'/'}>홈</Link>
+        <Link href={'/list'}>List</Link>
+      </nav>
       <div class="wrap">
         <h4 className="title">AppleFresh</h4>
         <p className="sub-title">by dev {name}</p>
