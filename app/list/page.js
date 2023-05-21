0eeb38ea670fb10e9ef66f268faa0c37030b2112
hey-ri <kwonhr01@naver.com>
2023-05-21 22:30:30 +0900

    create the list page

---
+++ app/list/page.js
@@ -0,0 +1,19 @@
+export default function List() {
+  return (
+    <div className="wrap">
+      <h4 className="title">상품목록</h4>
+      <section>
+        <div>
+          <p>상품 1</p>
+          <span>$40</span>
+        </div>
+      </section>
+      <section>
+        <div>
+          <p>상품 2</p>
+          <span>$70</span>
+        </div>
+      </section>
+    </div>
+  );
+}
