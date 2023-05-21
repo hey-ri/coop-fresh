a9212a3623ba569b49a1beefcf8991ba9e6e328c
hey-ri <kwonhr01@naver.com>
2023-05-21 22:34:37 +0900

    example routing

---
+++ app/abc/def/page.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Def() {
+  return <div>Def 페이지 입니다.</div>;
+}
---
+++ app/abc/page.js
@@ -0,0 +1,3 @@
+export default function Abc() {
+  return <div>Abc 컴포넌트 입니다</div>;
+}
