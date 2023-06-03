d7b2e3d66584596f86af51d2428d4cbdd8be2c66
hey-ri <kwonhr01@naver.com>
2023-06-03 23:40:08 +0900

    feature of client component , server component and import export syntax

---
+++ app/cart/data.jsx
@@ -0,0 +1,6 @@
+// export default age;
+export { age, name };
+let age = 20;
+let name = 20;
+/* 여러개를 내보내고 싶을 떈 export {} 꺼내쓸때는 import {꺼내기1, 꺼내기2} 
+한개만 내보내도 될땐 export defatul 내보낼이름 */
--- app/cart/page.js
+++ app/cart/page.js
@@ -1,3 +1,15 @@
+// 'use client';
+/* 윗줄을 맨 위에 선언해주면 클라이언트 컴포넌트가 됨
+클라잉언트 컴포넌트는 자바스크립트 기능 쓸 수 있음
+속도가 느림 (1. 자바스크립트 많이 필요 2. hydration 필요 *hydration이란? html 유저에게 보낸 후에 자바스크립트로 html 다시 읽고 분석))
+*/
+
+/* html 상에 자바스크립트 기능 못씀 , usestate useEffect 
+사용 불가 속도가 빠름 */
+
+/* 큰 페이지는 서버 컴포넌트로 작은 기능들은 클라이언트 컴포넌트로 사용하면 좋다. */
+import age from './data';
+
 export default function Cart() {
   return (
     <div className="wrap">
@@ -5,13 +17,20 @@ export default function Cart() {
       <div className="cart-item">
         <p>상품명</p>
         <p>$40</p>
-        <p>1개</p>
+        <p>{age}개</p>
       </div>
-      <div className="cart-item">
-        <p>상품명</p>
-        <p>$40</p>
-        <p>1개</p>
-      </div>
+      <CartItem />
+      <CartItem />
+    </div>
+  );
+}
+
+function CartItem() {
+  return (
+    <div className="cart-item">
+      <p>상품명</p>
+      <p>$40</p>
+      <p>1개</p>
     </div>
   );
 }
