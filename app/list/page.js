ff5a3c47a201359ea00cc9b0a4b6471ec4feb9c8
hey-ri <kwonhr01@naver.com>
2023-06-04 22:30:18 +0900

    each like button

--- app/list/page.js
+++ app/list/page.js
@@ -5,11 +5,17 @@ import { useState } from 'react';
 export default function List() {
   let product = ['Tomatoes', 'Pasta', 'Coconut'];
   const price = [10, 20, 50];
-  const [like, setLike] = useState(0);
 
+  const [like, setLike] = useState([0, 0, 0]);
+  /* usestate 는 기존의 값과 같으면 state를 변경해주지 않음 그래서 기존 state를 복사해서 씀, 
+  arr를 const arr = [1,2,3] 
+  const arr2 = arr 라고 하면 arr는 [1,2,3]을 가르키고 있는 화살표라 arr2도 그 화살표가 복사된다. 
+  [...arr]는 arr의 값을 복사하는 복사 문법이기 때문에 스프레드 문법으로 작성해야한다.({}[]두개 다 해당됨)
+  */
   return (
     <div className="wrap">
       <h4 className="title">상품목록</h4>
+
       {product.map((item, i) => (
         <section key={i}>
           <div>
@@ -18,15 +24,25 @@ export default function List() {
               {item}
               <span>$40</span>
             </h4>
-            <span>{like}</span>
+            <span>{like[i]}</span>
             <button
               onClick={() => {
-                setLike(like + 1);
+                let copy = [...like];
+                copy[i]++;
+                setLike(copy);
               }}
             >
               +
             </button>
-            <button onClick={() => setLike(like - 1)}>-</button>
+            <button
+              onClick={() => {
+                let copy = [...like];
+                copy[i]--;
+                setLike(copy);
+              }}
+            >
+              -
+            </button>
           </div>
         </section>
       ))}
